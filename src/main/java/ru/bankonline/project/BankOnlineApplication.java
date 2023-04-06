86ef60f1a3d049c1e9a44ce4cfbb69ec666cc058
evgeniy_tyshchenko <tyshenko.evgeni@yandex.ru>
2023-04-06 15:42:08 +0300

    Initial commit

---
+++ src/main/java/ru/bankonline/project/BankOnlineApplication.java
@@ -0,0 +1,13 @@
+package ru.bankonline.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankOnlineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BankOnlineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/bankonline/project/BankOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.bankonline.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankOnlineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
